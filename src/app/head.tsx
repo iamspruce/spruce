5dfb911f684ae509d2610d952946ff014e324ae7
iamspruce <iamspruce8@gmail.com>
2023-03-02 11:24:32 +0100

    added more SEO tags

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,13 +1,53 @@
 export default function Head() {
   return (
     <>
-      <title>Spruce Homepage</title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta charSet="utf-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width,initial-scale=1" />
+      <title>Spruce Emmanuel</title>
       <meta
         name="description"
-        content="Web Developer and online tech Educator, building web applications and creating content around JavaScript and everything web development"
+        content="Web Developer and online tech Educator, building web applications and creating content around JavaScript and everything web development."
       />
-      <link rel="icon" href="/favicon.ico" />
+      <meta property="og:title" content="Spruce Emmanuel" />
+      <meta
+        property="og:description"
+        content="Web Developer and online tech Educator, building web applications and creating content around JavaScript and everything web development."
+      />
+      <meta property="og:url" content="https://iamspruce.dev/" />
+      <meta property="og:image" content="/img/spruce.png" />
+      <meta property="og:type" content="website" />
+      <meta
+        name="twitter:title"
+        content="Web Developer and online tech Educator, building web applications and creating content around JavaScript and everything web development."
+      />
+      <meta name="twitter:card" content="summary" />
+      <meta name="author" content="Spruce Emmanuel" />
+      <meta property="og:site_name" content="Spruce Emmanuel" />
+      <meta property="og:locale" content="en_US" />
+      <meta name="twitter:site" content="@sprucekhalifa" />
+      <meta name="twitter:creator" content="@sprucekhalifa" />
+      <link rel="canonical" href="https://iamspruce.dev/" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png?v=1"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        href="/favicon-32x32.png?v=1"
+        sizes="32x32"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        href="/favicon-16x16.png?v=1"
+        sizes="16x16"
+      />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg?v=1" color="#1a1a1a" />
+      <link rel="shortcut icon" href="/favicon.ico?v=1" />
+      <link rel="manifest" href="/site.webmanifest" />
     </>
   );
 }
