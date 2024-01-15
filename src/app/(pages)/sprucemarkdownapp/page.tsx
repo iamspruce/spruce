d35a25a95bf8bafbe77d619a998e36e8207ce940
iamspruce <iamspruce8@gmail.com>
2024-01-15 07:56:22 +0100

    added sprucemarkdown logo

--- src/app/(pages)/sprucemarkdownapp/page.tsx
+++ src/app/(pages)/sprucemarkdownapp/page.tsx
@@ -1,8 +1,14 @@
+import { Logo } from "./logo";
+
 function Page() {
   return (
     <div className="wrapper_content">
       <div className=" mb_60 flex column align_center text_center justify_center gap_24">
-        <h1 className="h4">Spruce Markdown App 💡</h1>
+        <div className="sprucemarkdown_logo">
+          <div className="sprucemarkdown_logo_lights"></div>
+          <Logo />
+        </div>
+        <h1 className="h4">Spruce Markdown App</h1>
         <p>A Neat and Smart Markdown Editor</p>
       </div>
     </div>
